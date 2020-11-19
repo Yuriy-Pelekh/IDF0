e5e02e5593a0f85995fe3ec1d42266c409f18a5e
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2020-11-20 01:12:23 +0200

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,17 @@
+$headers = @{
+  "Authorization" = "$Env:APPVEYOR_TOKEN"
+  "Content-type" = "application/json"
+}
+$branch = $Env:APPVEYOR_REPO_BRANCH
+$pullRequestHeadBranch = $Env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH
+$pullRequestId = $Env:APPVEYOR_PULL_REQUEST_NUMBER
+echo "branch = $branch pullRequestHeadBranch = $pullRequestHeadBranch pullRequestId = $pullRequestId"
+$body = @{
+  accountName = "Yuriy-Pelekh"
+  projectSlug = "idf0-2"
+  branch = "$branch"
+  pullRequestHeadBranch = "$pullRequestHeadBranch"
+  pullRequestId = "pullRequestId"
+}
+$bodyAsJson = $body | ConvertTo-json
+Invoke-Restmethod -uri "https://ci.appveyor.com/api/builds" -Headers $headers -Method "Post" -Body $bodyAsJson
