fdb4ad6de3afcb80491f4fb8caa06a77f5ac83d0
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2020-11-20 02:48:26 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,15 +2,17 @@ $headers = @{
   "Authorization" = "$Env:APPVEYOR_TOKEN"
   "Content-type" = "application/json"
 }
+$accountName = 'Yuriy-Pelekh'
+$projectSlug = 'idf0-2'
 $branch = $Env:APPVEYOR_REPO_BRANCH
 $pullRequestHeadBranch = $Env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH
 $pullRequestId = $Env:APPVEYOR_PULL_REQUEST_NUMBER
 echo ">> pullRequestId = $pullRequestId branch = $branch pullRequestHeadBranch = $pullRequestHeadBranch <<"
 $body = @{
-  accountName = "Yuriy-Pelekh",
-  projectSlug = "idf0-2",
-  branch = "$branch",
-  pullRequestHeadBranch = "$pullRequestHeadBranch",
+  accountName = "$accountName"
+  projectSlug = "$projectSlug"
+  branch = "$branch"
+  pullRequestHeadBranch = "$pullRequestHeadBranch"
   pullRequestId = "$pullRequestId"
 }
 $bodyAsJson = $body | ConvertTo-json
