83738cc07c4ad0745d7924061e6c75d68b24b5ae
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2020-11-20 11:08:02 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -7,14 +7,14 @@ $projectSlug = 'idf0-2'
 $branch = $Env:APPVEYOR_REPO_BRANCH
 $pullRequestHeadBranch = $Env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH
 $pullRequestId = $Env:APPVEYOR_PULL_REQUEST_NUMBER
-echo ">> pullRequestId = $pullRequestId branch = $branch pullRequestHeadBranch = $pullRequestHeadBranch <<"
-$body = @{
-  accountName = "$accountName"
-  projectSlug = "$projectSlug"
-  branch = "$branch"
-  pullRequestHeadBranch = "$pullRequestHeadBranch"
-  pullRequestId = "$pullRequestId"
-}
-$bodyAsJson = $body | ConvertTo-json
-echo $bodyAsJson
-Invoke-Restmethod -uri "https://ci.appveyor.com/api/builds" -Headers $headers -Method "Post" -Body $bodyAsJson
+Write-Output ">> pullRequestId = $pullRequestId branch = $branch pullRequestHeadBranch = $pullRequestHeadBranch <<"
+$body = "{
+  'accountName': '$accountName',
+  'projectSlug': '$projectSlug',
+  'branch': '$branch',
+  'pullRequestHeadBranch': '$pullRequestHeadBranch',
+  'pullRequestId': '$pullRequestId'
+}"
+
+Write-Output $body
+Invoke-Restmethod -uri "https://ci.appveyor.com/api/builds" -Headers $headers -Method "Post" -Body $body
